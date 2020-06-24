d6d25c75a091778aad4ddb2b1f136ca8411040e5
Symion Edwards <symion.edwards@gmail.com>
2020-06-24 10:12:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil;
     false
-  else true
+  else board[index] == "X" || board[index] = "O";
+    true
   end
 end
